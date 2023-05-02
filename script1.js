1be17d4fbd197213226d7ec7c6a1ec009b6299d2
Basant Singh <51873983+Basant18@users.noreply.github.com>
2023-05-02 16:44:30 +0530

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,4 +1,4 @@
-const api_url = "http://www.omdbapi.com/?i=tt3896198&apikey=fc4b9e36&t=";
+const api_url = "https://www.omdbapi.com/?i=tt3896198&apikey=fc4b9e36&t=";
 const x = localStorage.getItem('movieNameMovie');
 console.log(x);
 
